bdf571bb1b54c6a1f20e031a65faa94ebdcd2282
Anthony Dahanne <anthony.dahanne@gmail.com>
2024-02-07 18:14:41 -0500

    Junit4 to Junit4 Integration Tests migration * add the extension registration declaratively (using an annotation)

--- integration-test/src/test/java/org/cloudfoundry/AbstractIntegrationTest.java
+++ integration-test/src/test/java/org/cloudfoundry/AbstractIntegrationTest.java
@@ -19,6 +19,7 @@
  import org.junit.jupiter.api.AfterEach;
  import org.junit.jupiter.api.BeforeEach;
  import org.junit.jupiter.api.TestInfo;
+ import org.junit.jupiter.api.extension.ExtendWith;
  import org.slf4j.Logger;
  import org.slf4j.LoggerFactory;
  import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,7 @@
  import static org.cloudfoundry.util.tuple.TupleUtils.consumer;
  
  @SpringJUnitConfig(classes = IntegrationTestConfiguration.class)
+ @ExtendWith(CloudFoundryVersionConditionalRule.class)
  public abstract class AbstractIntegrationTest {
  
      private final Logger logger = LoggerFactory.getLogger("cloudfoundry-client.test");
