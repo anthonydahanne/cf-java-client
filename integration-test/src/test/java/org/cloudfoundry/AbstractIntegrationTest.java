4aa8a32cb60dd1e74389ab5ca72b070d21d08aac
Anthony Dahanne <anthony.dahanne@gmail.com>
2024-02-08 11:07:04 -0500

    Do not use ExtendWith * when the extension has a mandatory field to initialize, because ExtendWith only works with default constructor

--- integration-test/src/test/java/org/cloudfoundry/AbstractIntegrationTest.java
+++ integration-test/src/test/java/org/cloudfoundry/AbstractIntegrationTest.java
@@ -19,14 +19,14 @@
  import org.junit.jupiter.api.AfterEach;
  import org.junit.jupiter.api.BeforeEach;
  import org.junit.jupiter.api.TestInfo;
- import org.junit.jupiter.api.extension.ExtendWith;
+ import org.junit.jupiter.api.extension.RegisterExtension;
  import org.slf4j.Logger;
  import org.slf4j.LoggerFactory;
  import org.springframework.beans.factory.annotation.Autowired;
  import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
  import reactor.core.publisher.Mono;
  import reactor.util.function.Tuple2;
- 
+
  import java.io.ByteArrayOutputStream;
  import java.io.File;
  import java.io.FileInputStream;
@@ -35,21 +35,24 @@
  import java.lang.reflect.Method;
  import java.util.Optional;
  import java.util.function.Consumer;
- 
+
  import static org.assertj.core.api.Assertions.assertThat;
  import static org.cloudfoundry.util.tuple.TupleUtils.consumer;
- 
+
  @SpringJUnitConfig(classes = IntegrationTestConfiguration.class)
- @ExtendWith(CloudFoundryVersionConditionalRule.class)
  public abstract class AbstractIntegrationTest {
- 
+
      private final Logger logger = LoggerFactory.getLogger("cloudfoundry-client.test");
- 
+
      public String testName;
- 
+
      @Autowired
      protected NameFactory nameFactory;
- 
+
+     @Autowired
+     @RegisterExtension
+     public CloudFoundryVersionConditionalRule cloudFoundryVersionConditionalRule;
+
      @BeforeEach
      public void testEntry(TestInfo testInfo) {
          Optional<Method> testMethod = testInfo.getTestMethod();
@@ -58,33 +61,33 @@
          }
          this.logger.debug(">> {} <<", getTestName());
      }
- 
+
      @AfterEach
      public final void testExit() {
          this.logger.debug("<< {} >>", getTestName());
      }
- 
+
      protected static Mono<byte[]> getBytes(String path) {
          try (InputStream in = new FileInputStream(new File("src/test/resources", path)); ByteArrayOutputStream out = new ByteArrayOutputStream()) {
              byte[] buffer = new byte[8192];
              int len;
- 
+
              while ((len = in.read(buffer)) != -1) {
                  out.write(buffer, 0, len);
              }
- 
+
              return Mono.just(out.toByteArray());
          } catch (IOException e) {
              throw new RuntimeException(e);
          }
      }
- 
+
      protected static <T> Consumer<Tuple2<T, T>> tupleEquality() {
          return consumer((expected, actual) -> assertThat(actual).isEqualTo(expected));
      }
- 
+
      private String getTestName() {
          return String.format("%s.%s", this.getClass().getSimpleName(), this.testName);
      }
- 
+
  }
